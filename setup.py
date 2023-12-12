451445e9a69fbfd5496ff9655864aa65efd1eab3
MvMukesh <mukeshmanral777@gmail.com>
2023-12-12 17:33:57 +0530

    adding setup file now

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="abusive-language",
+    version="0.1",
+    author="Manralai.in",
+    author_email="manralai.services@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
