4dc9eab295e2e4b7bc18ec81acb933078ed9057a
MvMukesh <mukeshmanral777@gmail.com>
2023-12-12 20:09:54 +0530

    updated logger code format

--- abusive_language/logger/__init__.py
+++ abusive_language/logger/__init__.py
@@ -1,8 +1,9 @@
-import logging
 import os
-
-from from_root import from_root
+import logging
 from datetime import datetime
+from from_root import from_root
+
+
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
@@ -14,5 +15,6 @@ LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
-    level=logging.DEBUG,
-)
+    level=logging.DEBUG,)
+
+### --- Manralai.in --- ###
