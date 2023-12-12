3c3f6f6924c8e2965a147788d2d75c1fc859d564
MvMukesh <mukeshmanral777@gmail.com>
2023-12-12 20:08:19 +0530

    updated ecception code

--- abusive_language/exception/__init__.py
+++ abusive_language/exception/__init__.py
@@ -1,13 +1,13 @@
 import os
 import sys
 
+
+
 def error_message_detail(error, error_detail:sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occurred python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
-    )
-
+        file_name, exc_tb.tb_lineno, str(error))
     return error_message
 
 
@@ -18,8 +18,9 @@ class CustomException(Exception):
         """
         super().__init__(error_message)
         self.error_message = error_message_detail(
-            error_message, error_detail=error_detail
-        )
+            error_message, error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
+    
+                ### --- Manralai.in --- ###
